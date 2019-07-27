9c8100ca77d09d1bdfed01f02640d932e9fddc71
Anggie Aziz <v-anggie.aziz@btpn.com>
2019-07-27 12:19:47 +0700

    chore: added env configuration

--- src/server.js
+++ src/server.js
@@ -3,7 +3,10 @@ const buildApp = require('./build-app')
 const startServer = async () => {
   const app = await buildApp();
 
-  app.listen({ port: 3000 }, () => {
+  app.listen({
+    host: process.env.SERVICE_HOST,
+    port: process.env.SERVICE_PORT
+  }, () => {
     console.log('Server is up and running');
   });
 };
