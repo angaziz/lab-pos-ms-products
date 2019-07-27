e9eca1d81d1bfff2d752edb44adb8101ba57f704
Anggie Aziz <v-anggie.aziz@btpn.com>
2019-07-27 20:41:32 +0700

    feat: non-nullable products

--- src/modules/product/resolvers/queries/get-products.js
+++ src/modules/product/resolvers/queries/get-products.js
@@ -2,7 +2,7 @@ const {
   response
 } = require('../../../../transformations');
 
-module.exports = async (_root, args, context, info) => {
+module.exports = async (_root, args, context) => {
   try {
     const query = context.db.models.Product.find().skip(args.offset).limit(args.limit);
     const result = await query.exec();
